a73ca38c1eb60e0acb64127bd2cbf8d5c9922f5f
M1yank <mbansal1_be19@thapar.edu>
2022-07-30 18:33:44 +0530

    main

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import cv2
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
+import time
 
 cap=cv2.VideoCapture(0)
 
 cap.set(3, 640)
 cap.set(4, 480)
 
-detector= HandDetector(maxHands=2)
+### hand detector
+detector= HandDetector(maxHands=1)
+
+
+timer = 0
+stateResult = False
+startGame = False
+
+
 
 while True:
     #read background image
@@ -21,13 +30,42 @@ while True:
     imgScaled = imgScaled[:,51:353]
     
     #flip the cam
-    imgScaled = cv2.flip(imgScaled, 1)
+    # imgScaled = cv2.flip(imgScaled, 1)
 
     #find hands
     hands, img = detector.findHands(imgScaled)
+
+    if startGame:
+        
+        if stateResult is False:
+            timer = time.time() - InitialTime
+            cv2.putText(imgBG, str(int(timer)), (450, 340), cv2.FONT_HERSHEY_PLAIN, 6, (0,0,0), 4)
+            if timer>3:
+                stateResult = True
+                timer = 0 
+
+                if hands:
+                    hand=hands[0]
+                    fingers = detector.fingersUp(hand)
+
+                    if fingers==[0, 0, 0, 0, 0]:    #rock
+                        playerMove = 1
+                    if fingers==[1, 1, 1, 1, 1]:    #paper
+                        playerMove = 2
+                    if fingers==[0, 1, 1, 0, 0]:    #scissor
+                        playerMove = 3
+
+                    imgAI=cv2.imread(f'Resources/1.png');
+                    imgBG = cvzone.overlayPNG(imgBG, imgAI, ())
+
+
+                    print(playerMove) 
 
 
     #embed cam into background image
     imgBG[178:481, 596:898] = imgScaled
     cv2.imshow("BG", imgBG)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord('s'):
+        startGame = True
+        InitialTime = time.time()   
